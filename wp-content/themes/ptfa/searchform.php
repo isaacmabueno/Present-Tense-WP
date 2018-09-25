480c8cdda1cd477eec48c69460f95b8abdb2957d
isaacmabueno <isaacmabueno@gmail.com>
2018-09-24 22:20:45 -0500

    styling button

--- wp-content/themes/ptfa/searchform.php
+++ wp-content/themes/ptfa/searchform.php
@@ -1,13 +1,11 @@
 <section class="custom_search_form">
-    <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label>
-        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-        <input type="search" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-            value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
-    <input type="submit" class="search-submit"
-        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-</form>
+    <div class="" role="search">
+        <form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <div>
+                <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+                <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+                <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+            </div>
+        </form>
+    </div>
 </section>
